2fafc6819c1a5f6b371600337d4003d6ec961888
Naveen Prakash <chnaveenprakash@gmail.com>
2024-06-26 23:27:35 +0530

    TMDB API data fetch && built MainContainer

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
 
 
   return (
-    <div className='absolute px-8 py-2 w-screen bg-gradient-to-b from-black z-10 flex justify-between'>
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
         src = {LOGO}
